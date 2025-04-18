374d693bea3051e725c63205f7d78dce07783da0
Mohammad <amirarsalan1380a@gmail.com>
2025-04-18 14:24:48 -0700

    Add custom styles for single product booking buttons and date picker

--- functions.php
+++ functions.php
@@ -4,3 +4,8 @@
 * Custom Post Types & Custom Taxonomies
 */
 require get_template_directory() . '/inc/post-types-taxonomies.php';
+
+function load_theme_styles() {
+    wp_enqueue_style('theme-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'load_theme_styles');
