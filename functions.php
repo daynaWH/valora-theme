836a7af37fcefc4219f3a6ed17c3946aa77bb7c0
DAYNA <dayna.cho119@gmail.com>
2025-05-05 13:56:04 -0400

    Removed duplicated theme styles loading on the front end

--- functions.php
+++ functions.php
@@ -22,11 +22,6 @@ add_action( 'wp_enqueue_scripts', 'valora_enqueues' );
 // Load Custom Post Types & Custom Taxonomies
 require get_template_directory() . '/inc/post-types-taxonomies.php';
 
-function load_theme_styles() {
-    wp_enqueue_style('theme-style', get_stylesheet_uri());
-}
-add_action('wp_enqueue_scripts', 'load_theme_styles');
-
 // Load Custom blocks
 require get_theme_file_path() . '/valora-blocks/valora-blocks.php';
 
